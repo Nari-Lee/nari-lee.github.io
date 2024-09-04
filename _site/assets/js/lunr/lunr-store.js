cfec24420c8c94fc13436218b851990ecc923e05
Nari-Lee <serika9009@gmail.com>
2024-09-04 09:12:29 +0900

    Servlet 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,25 +1,31 @@
 var store = [{
+        "title": "블로그 계획",
+        "excerpt":"GitHub Pages의 github.io 블로그 시작하기로 하였다. 블로그 계획을 한번 세워보았다. h2 태그로 제목을 쓰면 TOC 테이블로 오른쪽 사이드바에 보여줄 수 있다.   1. 기술 공부하기   공부하고 블로그 글로 게시하자.   2. 프로젝트 소개하기   개인적으로 진행하는 프로젝트를 진행해보고 결과를 소개해보자.  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "/blog/Blog-Plan/",
+        "teaser": null
+      },{
         "title": "github.io 블로그 시작",
         "excerpt":"GitHub Blog 서비스인 github.io 블로그 시작하기로 했다. GitHub Blog 서비스의 이름은 Pages이다.   테마는 minimal-mistakes이다.  ","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "/blog/Hello-world/",
         "teaser": null
       },{
-        "title": "블로그 계획",
-        "excerpt":"GitHub Pages의 github.io 블로그 시작하기로 하였다. 블로그 계획을 한번 세워보았다. h2 태그로 제목을 쓰면 TOC 테이블로 오른쪽 사이드바에 보여줄 수 있다.   1. 기술 공부하기   공부하고 블로그 글로 게시하자.   2. 프로젝트 소개하기   개인적으로 진행하는 프로젝트를 진행해보고 결과를 소개해보자.  ","categories": ["Blog"],
-        "tags": ["Blog"],
-        "url": "/blog/blog-plan/",
-        "teaser": null
-      },{
         "title": "마크다운 문법 가이드",
         "excerpt":"마크다운(markdown)이란 마크업 언어의 일종으로 흔히 github의 “README.md”에서 볼 수 있다. 확장자는 .md 또는 .markdown을 사용한다. 1. 제목 작성하기 제목은 ‘#’ 기호를 사용해 표현합니다. ‘#’의 개수에 따라 크기가 달라진다. # 제목 1 ## 제목 2 ### 제목 3 #### 제목 4 ##### 제목 5 ###### 제목 6 위와 같이 입력하면 아래처럼...","categories": ["Study"],
         "tags": ["Blog","GitHub pages"],
-        "url": "/study/markdown/",
+        "url": "/study/Markdown/",
         "teaser": null
       },{
         "title": "JDBC(Java Database Connectivity)",
         "excerpt":"JDBC(Java Database Connectivity)는 Java 애플리케이션에서 데이터베이스에 접근할 수 있게 해주는 Java API이다. 이를 통해 개발자는 다양한 관계형 데이터베이스에 연결하고 SQL 쿼리를 실행할 수 있다. 1. 주요 특징 표준화된 인터페이스 : JDBC는 자바 플랫폼의 표준 부분으로, 일관된 API를 제공한다. 이를 통해 개발자는 특정 데이터 베이스에 종속되지 않는 코드를 작성 할 수...","categories": ["Study"],
         "tags": ["Study","Java","SQL","JDBC"],
         "url": "/study/JDBC/",
+        "teaser": null
+      },{
+        "title": "MyBatis",
+        "excerpt":"MyBatis는 자바 퍼시스턴스 프레임워크로, 데이터베이스 프로그래밍을 간소화하고 SQL, 저장프로시저 그리고 몇가지 고급 매핑을 지원한다.   1. 주요 특징        ","categories": ["Study"],
+        "tags": ["Study","Java","SQL","MyBatis"],
+        "url": "/study/MyBatis/",
         "teaser": null
       }]
